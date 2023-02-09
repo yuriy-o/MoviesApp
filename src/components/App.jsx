da566928e72eb1c1680699a6ea638188b454f182
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-02-10 00:13:25 +0200

    add header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import { Header, Header_Link } from './App.styled';
+import { Header, HeaderLink } from './App.styled';
 
 export const App = () => {
   return (
     <>
       <Header>
-        <Header_Link href="#">Home</Header_Link>
-        <Header_Link href="#">Movies</Header_Link>
+        <HeaderLink href="#">Home</HeaderLink>
+        <HeaderLink href="#">Movies</HeaderLink>
       </Header>
       <main> </main>
     </>
--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -2,7 +2,34 @@ import styled from '@emotion/styled';
 
 export const Header = styled.header`
   padding: 20px 40px;
+
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `;
-export const Header_Link = styled.a`
-  color: red;
+export const HeaderLink = styled.a`
+  position: relative;
+  text-decoration: none;
+  color: black;
+  font-size: 30px;
+  padding: 10px;
+  /* font-size: 2rem; */
+
+  :not(:last-child) {
+    margin-right: 10px;
+  }
+
+  ::before {
+    content: '';
+    position: absolute;
+    height: 3px;
+    left: 0px;
+    bottom: 0px;
+    transition: 0.6s;
+    width: 0%;
+    background: linear-gradient(to right, #ff0000, #fff200, #1e9600);
+  }
+
+  :hover::before,
+  :focus::before {
+    width: 100%;
+  }
 `;
